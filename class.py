e43250276e337f6a8b5558e0c36d98992187d6f9
sweta <swetat007@protonmail.com>
2022-12-28 12:39:51 +0100

    Two methods added one with (0 parameters) and one with 1 parameter.

--- class.py
+++ class.py
@@ -3,16 +3,40 @@ class Koffeemachine :
     weight= 0
     color = ""
     brand = ""
+    start = 0
+    name = ""
 
+    def starttime(self):
+        print(self.name + " starts after " + str(self.start) + " seconds.")
+        print("it takes some seconds.")
+
+    def broyer(self):
+        print( self.name + " can grind the coffee grains.")
+
+    def stop(self, stop):
+        print(self.name + " stops after " + str(stop) + " milliseconds.")
+    
 machine1 = Koffeemachine()
 machine2 = Koffeemachine()
 
 machine1.color = "Yellow"
-machine2.color = "Red"
 machine1.brand ="Nestlé"
+machine1.weight = 2
+machine1.start = 20
+machine1.name = "Saeco-XP"
+
+machine1.stop(20)
+machine1.starttime()
+
+machine2.color = "Red"
 machine2.brand = "Godrege"
 machine2.weight = 2.3
-machine1.weight = 1.2
+machine2.start = 10
+machine2.name = "Jura x-10"
+machine2.starttime()
+machine2.stop(40)
 
-print("My first coffee machine's is from " +  machine1.brand + " company & it was " + machine1.color + ".")
-print("Now my coffee machine's is from " +  machine2.brand + " company & it is " + machine2.color + ".")
+
+
+# print("My first coffee machine's is from " +  machine1.brand + " company & it was " + machine1.color + ".")
+# print("Now my coffee machine's is from " +  machine2.brand + " company & it is " + machine2.color + ".")
