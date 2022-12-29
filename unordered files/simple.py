c077d2353a5694b62cff4269c26cc008be9be55e
sweta <swetat007@protonmail.com>
2022-12-29 07:38:05 +0100

    class exercise

---
+++ unordered files/simple.py
@@ -0,0 +1,19 @@
+i = int(input("enter your input"))
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+# fruits = {"apple", "banana", "cherry"}
+
+# more_fruits = ["orange", "mango", "grapes"]
+
+# set = fruits.aadd(more_fruits)
+# print(set)
+
+
+
+ 
+
+
