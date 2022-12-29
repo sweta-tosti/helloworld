b82811e8be33a24904986f93b512bd8a5880ae18
sweta <swetat007@protonmail.com>
2022-12-29 09:56:57 +0100

    Array accessing value

---
+++ JavaScriptInDepth/arrays.js
@@ -0,0 +1,17 @@
+
+var values = [13, "Hello", -5.6, "blue"];
+
+// console.log(values);
+
+var firstElement = values[0];
+var secondElement = values[1];
+var thirdElement = values[2];
+
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(values[7]);
+
+//update the value
+values[1] = "Good bye";
+
+console.log(values)
