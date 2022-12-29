cb0fc6949fcac578bae0b5754f67641e20fd4a0c
sweta <swetat007@protonmail.com>
2022-12-29 09:02:38 +0100

    Js variable & string

---
+++ JavaScriptInDepth/variableData.js
@@ -0,0 +1,12 @@
+
+var price = 12.15 ; // a number
+var message = "Something fun"; // a string
+
+var temperature;
+console.log(temperature)
+
+temperature = 2.3
+console.log(temperature)
+
+temperature = null
+console.log(temperature)
